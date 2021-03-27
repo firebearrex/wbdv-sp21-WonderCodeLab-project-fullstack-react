c8e15b20339e5dddb63166607c20a911369f372e
chloeduan <chloe@chloes-MacBook-Pro.local>
2021-03-27 03:40:08 -0700

    Initialize project using Create React App

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import React from "react";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bay Area Events</h1>
+    </div>
+  );
+}
+
+export default App;
